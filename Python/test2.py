ce8dec43b3017a3cd570c7fc5f17e8e4c3ef38f6
Jonas Pinson <jonas.pinson@gmail.com>
2018-01-12 18:49:34 +0100

    teeny tiny bit of algorithm

--- Python/test2.py
+++ Python/test2.py
@@ -3,7 +3,12 @@ import time
 import argparse
 from naoqi import ALProxy
 
-def main(robotIP, PORT = 9559):
+import math
+
+
+
+
+def main(robotIP = "172.30.248.162", PORT = 9559):
     try:
         motionProxy = ALProxy("ALMotion", robotIP, PORT)
     except Exception, e:
@@ -27,16 +32,16 @@ def main(robotIP, PORT = 9559):
     # Example showing multiple trajectories
     # Interpolates the head yaw to 1.0 radian and back to zero in 2.0 seconds
     # while interpolating HeadPitch up and down over a longer period.
-    names      = ["RShoulderPitch", "RShoulderRoll", "RElbowRoll","RElbowYaw"]
+    names      = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll"]
     # Each joint can have lists of different lengths, but the number of
     # angles and the number of times must be the same for each joint.
     # Here, the second joint ("HeadPitch") has three angles, and
     # three corresponding times.
-    angleLists  = [[50.0*almath.TO_RAD, 10.0*almath.TO_RAD],
-                   [-30.0*almath.TO_RAD, 30.0*almath.TO_RAD, -10.0*almath.TO_RAD],
-                   [0.0, 88.5*almath.TO_RAD, 0.0],
-                   [70*almath.TO_RAD, 0.0*almath.TO_RAD, 70*almath.TO_RAD]]
-    timeLists   = [[1.0, 2.0], [ 1.0, 2.0, 3.0], [1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]
+    angleLists  = [[(80-0.89)*almath.TO_RAD, (80-22.88)*almath.TO_RAD],
+                   [(-0.5-15.15)*almath.TO_RAD, (-0.5-69.56)*almath.TO_RAD],
+                   [(119.5-82.05)*almath.TO_RAD, (119.5-139.59)*almath.TO_RAD],
+                   [(-180+160.94)*almath.TO_RAD, (-180+62.38)*almath.TO_RAD]]
+    timeLists   = [[1.0, 3.0], [ 1.0, 3.0], [1.0, 3.0], [1.0, 3.0]]
     isAbsolute  = True
     motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute)
 
@@ -48,7 +53,7 @@ def main(robotIP, PORT = 9559):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="172.30.248.108",
+    parser.add_argument("--ip", type=str, default="172.30.248.162",
                         help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")
---
+++ Python/testalgo.py
@@ -0,0 +1,94 @@
+import math
+
+
+def angleRshoulderPitch(x1,y1,z1,x2,y2,z2):
+    print("RShoulderPitch")
+    a1=(x2-x2)**2+(y1-y2)**2 + (z1-z2)**2
+    lineA= a1 ** 0.5
+    # print( "line A: " + str(lineA) +" m")
+    a2=(x2-x2)**2+(y2-y2)**2 + (z1-z2)**2
+    lineB= a2 ** 0.5
+    # print( "line B: " + str(lineB) +" m")
+    a3=(x2-x2)**2+(y1-y2)**2 + (z1-z1)**2
+    lineC= a3 ** 0.5
+    # print( "line C: " + str(lineC) +" m")
+    cosB = (pow(lineA, 2) + pow(lineC,2) - pow(lineB,2))/(2*lineA*lineC)
+    acosB = math.acos(cosB)
+    print( "Radian B: " + str(float(format(acosB, '.3f'))) +" rad")
+    angleB = float(format(math.degrees(acosB), '.2f'))  
+    print( "Angle B: " + str(angleB) +" deg")
+
+def angleRshoulderRoll(x1,y1,z1,x2,y2,z2):
+    print("RShoulderRoll")
+    a1=(x1-x2)**2+(y1-y2)**2 + (z1-z1)**2
+    lineA= a1 ** 0.5
+    # print( "line A: " + str(lineA) +" m")    
+    a2=(x2-x1)**2+(y2-y2)**2 + (z1-z1)**2
+    lineB= a2 ** 0.5
+    # print( "line B: " + str(lineB) +" m")
+    a3=(x1-x1)**2+(y1-y2)**2 + (z1-z1)**2
+    lineC= a3 ** 0.5
+    # print( "line C: " + str(lineC) +" m")
+    cosB = (pow(lineA, 2) + pow(lineC,2) - pow(lineB,2))/(2*lineA*lineC)
+    acosB = math.acos(cosB)
+    print( "Radian B: " + str(float(format(acosB, '.3f'))) +" rad")
+    angleB = float(format(math.degrees(acosB), '.2f'))  
+    print( "Angle B: " + str(angleB) +" deg")
+
+def angleRElbowRoll(x1,y1,z1,x2,y2,z2,x3,y3,z3):
+    print("RElbowRoll")
+    a1=(x1-x2)**2+(y1-y2)**2 + (z1-z2)**2
+    lineA= a1 ** 0.5
+    # print( "line A: " + str(lineA) +" m")    
+    a2=(x2-x3)**2+(y2-y3)**2 + (z2-z3)**2
+    lineB= a2 ** 0.5
+    # print( "line B: " + str(lineB) +" m")
+    a3=(x1-x3)**2+(y1-y3)**2 + (z1-z3)**2
+    lineC= a3 ** 0.5
+    # print( "line C: " + str(lineC) +" m")
+    cosB = (pow(lineA, 2) + pow(lineB,2) - pow(lineC,2))/(2*lineA*lineB)
+    acosB = math.acos(cosB)
+    print( "Radian B: " + str(float(format(acosB, '.3f'))) +" rad")
+    angleB = float(format(math.degrees(acosB), '.2f'))  
+    print( "Angle B: " + str(angleB) +" deg")
+
+def angleRElbowYaw(x1,y1,z1,x2,y2,z2):
+    print("RElbowYaw")
+    a1=(x1-x2)**2+(y1-y2)**2 + (z1-z2)**2
+    lineA= a1 ** 0.5
+    # print( "line A: " + str(lineA) +" m")    
+    a2=(x2-x2)**2+(y2-y1)**2 + (z2-z1)**2
+    lineB= a2 ** 0.5
+    # print( "line B: " + str(lineB) +" m")
+    a3=(x1-x2)**2+(y1-y1)**2 + (z1-z1)**2
+    lineC= a3 ** 0.5
+    # print( "line C: " + str(lineC) +" m")
+    cosB = (pow(lineA, 2) + pow(lineC,2) - pow(lineB,2))/(2*lineA*lineC)
+    acosB = math.acos(cosB)
+    print( "Radian B: " + str(float(format(acosB, '.3f'))) +" rad")
+    angleB = float(format(math.degrees(acosB), '.2f'))  
+    print( "Angle B: " + str(angleB) +" deg")
+
+
+
+# list_of_joints = []
+
+# text_file = open("BoneOrientationsTest.txt", "r")
+# lines = text_file.read().split(',')
+# text_file.close()
+
+# i=0
+# while i<=77:
+#     angleRshoulderPitch(float(lines[i]),float(lines[i+1]),float(lines[i+2]),float(lines[i+3]),float(lines[i+4]),float(lines[i+5]))
+#     angleRshoulderRoll(float(lines[i]),float(lines[i+1]),float(lines[i+2]),float(lines[i+3]),float(lines[i+4]),float(lines[i+5]))
+#     i+=6
+
+angleRshoulderPitch(0.06092434,0.5326838,2.438355, 0.1362722,0.2543808,2.442603)
+angleRshoulderRoll(0.06092434,0.5326838,2.438355, 0.1362722,0.2543808,2.442603)
+angleRElbowRoll(0.06092434,0.5326838,2.438355, 0.1362722,0.2543808,2.442603, 0.171876,0.01064131,2.367382)
+angleRElbowYaw(0.1362722,0.2543808,2.442603, 0.171876,0.01064131,2.367382)
+print("------------------")
+angleRshoulderPitch(0.03568992,0.5751851,2.444758, 0.274239,0.6649151,2.406888)
+angleRshoulderRoll(0.03568992,0.5751851,2.444758, 0.274239,0.6649151,2.406888)
+angleRElbowRoll(0.03568992,0.5751851,2.444758, 0.274239,0.6649151,2.406888, 0.3891994,0.8548476,2.296391)
+angleRElbowYaw(0.274239,0.6649151,2.406888, 0.3891994,0.8548476,2.296391)
