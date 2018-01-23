cb5ba356ea3a64a8e633e6841ca45e72d7a5937b
Jonas Pinson <jonas.pinson@gmail.com>
2018-01-23 12:20:09 +0100

    start json

--- Python/mqtt.py
+++ Python/mqtt.py
@@ -6,7 +6,7 @@ import time
 import argparse
 from naoqi import ALProxy
 
-# import main from test2
+# Global variables
 listAngles = []
 shoulderLeft = []
 elbowLeft = []
@@ -15,56 +15,57 @@ shoulderRight = []
 elbowRight = []
 wristRight = []
 t = 0
+test = []
 
 
 def sendrobot(anglelist, robotIP="172.30.248.133", PORT=9559):
     try:
         try:
-            motionProxy = ALProxy("ALMotion", robotIP, PORT)
+            motionProxy = ALProxy("ALMotion", robotIP, PORT) #creates proxy to call specific functions
         except Exception, e:
             print "Could not create proxy to AlMotion"
             print "Error was: ", e
         try:
-            postureProxy = ALProxy("ALRobotPosture", robotIP, 9559)
+            postureProxy = ALProxy("ALRobotPosture", robotIP, 9559) #creates proxy to call specific functions
         except Exception, e:
             print "Could not create proxy to ALRobotPosture"
             print "Error was: ", e
 
-        global t
+        global t # uses global variable t
 
-        if (t == 0):
-            motionProxy.setStiffnesses("Body", 0.0)
-            postureProxy.goToPosture("StandInit", 0.5)        
+        if (t == 0): # if it is the first time the robot is called upon
+            motionProxy.setStiffnesses("Body", 0.0) # unstiffens the joints
+            postureProxy.goToPosture("StandInit", 0.5) # gets the robot into his initial standing position
 
         names = ["RShoulderPitch", "RShoulderRoll", "RElbowRoll", "RElbowYaw",  "LShoulderPitch", "LShoulderRoll", "LElbowRoll", "LElbowYaw"]
+        #list of joints that will get changed
 
-        angleLists = [[(anglelist[len(anglelist) - 8]) * almath.TO_RAD],
-                      [(anglelist[len(anglelist) - 7]) * almath.TO_RAD],
-                      [(anglelist[len(anglelist) - 6]) * almath.TO_RAD],
+        angleLists = [[(anglelist[len(anglelist) - 8]) * almath.TO_RAD], # all the coordinates are saved in one big list
+                      [(anglelist[len(anglelist) - 7]) * almath.TO_RAD], # and in a specific order (see list of joints)
+                      [(anglelist[len(anglelist) - 6]) * almath.TO_RAD], # this gets them out of that list and sent to the right joint
                       [(anglelist[len(anglelist) - 5]) * almath.TO_RAD],
                       [(anglelist[len(anglelist) - 4]) * almath.TO_RAD],
                       [(anglelist[len(anglelist) - 3]) * almath.TO_RAD],
                       [(anglelist[len(anglelist) - 2]) * almath.TO_RAD],
                       [(anglelist[len(anglelist) - 1]) * almath.TO_RAD]]
-        timeLists = [[0.4], [0.4], [0.4], [0.4], [0.4], [0.4], [0.4], [0.4]]
-        isAbsolute = True
-        motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute)
-        t += 1  
-    except Exception:
-        pass
-    except (KeyboardInterrupt, SystemExit):
-        postureProxy.goToPosture("StandInit", 0.5)
-        motionProxy.setStiffnesses("Body", 1.0)
-        raise
+        timeLists = [[0.4], [0.4], [0.4], [0.4], [0.4], [0.4], [0.4], [0.4]] # sets the time the robot has to get to the joint location (when you give more than one coordinate for a joint, you have to give more than one timestamp for that same joint!)
+        isAbsolute = True # kindoff is deprecated, but makes the joint positions absolute and not relative
+        motionProxy.angleInterpolation(names, angleLists, timeLists, isAbsolute) #the function talks with the robot
+        t += 1 # global t gets added by 1 so the joints dont get unstiffened again and the robot does not get put in its initial position
+    except Exception: # checks for any and all errors
+        pass # ignores every single one of them, except keyboardInterupt and SystemExit
+    except (KeyboardInterrupt, SystemExit): # when the program gets terminated
+        postureProxy.goToPosture("StandInit", 0.5) # set the robot in its initial position
+        motionProxy.setStiffnesses("Body", 1.0) # stiffen the joints
+        raise #actually quit
 
-def angleRShoulderPitch(x2, y2, z2, x1, y1, z1):
+def angleRShoulderPitch(x2, y2, z2, x1, y1, z1): #calulates the Shoulderpitch value for the Right shoulder by using geometry
     if(y2<y1):
-        angle = math.atan(abs(y2 - y1) / abs(z2 - z1))
+        angle = math.atan(abs(y2 - y1) / abs(z2 - z1)) 
         angle = math.degrees(angle)
         angle = -(angle)
         if(angle<-118):
             angle = -117
-        print("ik zit hier in en mijn hoek is {0}").format(angle)
         return angle
     else:
         angle = math.atan((z2-z1)/(y2-y1))
@@ -72,7 +73,7 @@ def angleRShoulderPitch(x2, y2, z2, x1, y1, z1):
         angle = 90-angle
         return angle
 
-def angleRShoulderRoll(x2, y2, z2, x1, y1, z1):
+def angleRShoulderRoll(x2, y2, z2, x1, y1, z1): #calulates the ShoulderRoll value for the Right shoulder by using geometry
     if(z2<z1):
         test = z2
         anderetest = z1
@@ -85,7 +86,7 @@ def angleRShoulderRoll(x2, y2, z2, x1, y1, z1):
     angle = math.degrees(angle)
     return angle
 
-def angleLShoulderPitch(x2, y2, z2, x1, y1, z1):
+def angleLShoulderPitch(x2, y2, z2, x1, y1, z1): #calulates the Shoulderpitch value for the Left shoulder by using geometry
     if (y2 < y1):
         angle = math.atan(abs(y2 - y1) / abs(z2 - z1))
         angle = math.degrees(angle)
@@ -99,7 +100,7 @@ def angleLShoulderPitch(x2, y2, z2, x1, y1, z1):
         angle = 90 - angle
         return angle
 
-def angleLShouderRoll(x2, y2, z2, x1, y1, z1):
+def angleLShouderRoll(x2, y2, z2, x1, y1, z1): #calulates the ShoulderRoll value for the Left shoulder by using geometry
     if (z2 < z1):
         test = z2
         anderetest = z1
@@ -112,151 +113,138 @@ def angleLShouderRoll(x2, y2, z2, x1, y1, z1):
     angle = math.degrees(angle)
     return angle
 
-def angleRElbowYaw(x2, y2, z2, x1, y1, z1,shoulderpitch):
+def angleRElbowYaw(x2, y2, z2, x1, y1, z1,shoulderpitch): #calulates the ElbowYaw value for the Right elbow by using geometry
     if(abs(y2-y1)<0.2 and abs(z2-z1) < 0.2 and (x1<x2) ):
-        print ("Ra")
         return 0
     elif(abs(x2-x1)<0.1 and abs(z2-z1)<0.1 and (y1>y2)):
-        print("Rb")
         return 90
     elif(abs(x2-x1)<0.1 and abs(z2-z1)<0.1 and (shoulderpitch > 50)):
-        print("Rc")
         return 90
     elif(abs(y2-y1)<0.1 and abs(z2-z1)<0.1 and (shoulderpitch < 50)):
-        print("Rd")
         return 0
     elif(abs(x2-x1)<0.1 and abs(y2-y1)<0.1 and (shoulderpitch > 50)):
-        print("Re")
         return 90
     else:
         angle = math.atan((z2 - z1) / (y2 - y1))
         angle = math.degrees(angle)
         angle = - angle + (shoulderpitch)
         angle = - angle
-        print("Rf: " + str(angle))
         return angle
 
 
-def angleRElbowRoll(x3, y3, z3, x2, y2, z2, x1, y1, z1):
-    a1=(x3-x2)**2+(y3-y2)**2 + (z3-z2)**2
-    lineA= a1 ** 0.5
+def angleRElbowRoll(x3, y3, z3, x2, y2, z2, x1, y1, z1): #calulates the ElbowRoll value for the Right elbow by using geometry
+    a1=(x3-x2)**2+(y3-y2)**2 + (z3-z2)**2 
+    lineA= a1 ** 0.5                        # calculates length of line between 2 3D coordinates
     b1=(x2-x1)**2+(y2-y1)**2 + (z2-z1)**2
-    lineB= b1 ** 0.5
+    lineB= b1 ** 0.5                        # calculates length of line between 2 3D coordinates
     c1=(x1-x3)**2+(y1-y3)**2 + (z1-z3)**2
-    lineC= c1 ** 0.5
+    lineC= c1 ** 0.5                        # calculates length of line between 2 3D coordinates
 
     cosB = (pow(lineA, 2) + pow(lineB,2) - pow(lineC,2))/(2*lineA*lineB)
     acosB = math.acos(cosB)
     angle = math.degrees(acosB)
     angle = 180 - angle
-    print("RElbowRoll", angle)
     return angle
 
 
-def angleLElbowYaw(x2, y2, z2, x1, y1, z1, shoulderpitch):
+def angleLElbowYaw(x2, y2, z2, x1, y1, z1, shoulderpitch): #calulates the ElbowYaw value for the Left elbow by using geometry
     if(abs(y2-y1)<0.2 and abs(z2-z1) < 0.2 and (x1>x2) ):
-        print ("La")
         return 0
     elif(abs(x2-x1)<0.1 and abs(z2-z1)<0.1 and (y1>y2)):
-        print("Lb")
         return -90
     elif(abs(x2-x1)<0.1 and abs(z2-z1)<0.1 and (shoulderpitch > 50)):
-        print("Lc")
         return -90
     elif(abs(y2-y1)<0.1 and abs(z2-z1)<0.1 and (shoulderpitch > 50)):
-        print("Ld")
         return 0
     elif(abs(x2-x1)<0.1 and abs(y2-y1)<0.1 and (shoulderpitch > 50)):
-        print("LE")
         return -90
     else:
         angle = math.atan((z2 - z1) / (y2 - y1))
         angle = math.degrees(angle)
         angle = - angle + (shoulderpitch)
         angle = - angle
-        print("Lf: " + str(angle))
         return angle
 
-def angleLElbowRoll(x3, y3, z3, x2, y2, z2, x1, y1, z1):
+def angleLElbowRoll(x3, y3, z3, x2, y2, z2, x1, y1, z1): #calulates the ElbowRoll value for the Left elbow by using geometry
 
     a1=(x3-x2)**2+(y3-y2)**2 + (z3-z2)**2
-    lineA= a1 ** 0.5
+    lineA= a1 ** 0.5                        # calculates length of line between 2 3D coordinates
     b1=(x2-x1)**2+(y2-y1)**2 + (z2-z1)**2
-    lineB= b1 ** 0.5
+    lineB= b1 ** 0.5                        # calculates length of line between 2 3D coordinates
     c1=(x1-x3)**2+(y1-y3)**2 + (z1-z3)**2
-    lineC= c1 ** 0.5
+    lineC= c1 ** 0.5                        # calculates length of line between 2 3D coordinates
 
     cosB = (pow(lineA, 2) + pow(lineB,2) - pow(lineC,2))/(2*lineA*lineB)
     acosB = math.acos(cosB)
     angle = math.degrees(acosB)
     angle = -180+ angle
-    print("LElbowRoll", angle)
     return angle
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc): # connects with mqtt and subscribes to /Sandro
     print("Connected with result code " + str(rc))
+    # client.subscribe(raw_input("Geef je MQTT Topic:"))
     client.subscribe("/Sandro")
 
-def on_message(client, userdata, msg):
-    # print(msg.topic+" "+str(msg.payload))
+def on_message(client, userdata, msg): # Checks the mqtt message it receives and processes the json
     payload = json.loads(msg.payload.decode('utf-8'))
-    for i in payload:
-        # print(i['jointname'])
-        if i['jointname'] == "ShoulderLeft":
-            shoulderLeft = i['coordinates']
-            # print(i['coordinates'])
-        if i['jointname'] == "ElbowLeft":
-            elbowLeft = i['coordinates']
-            # print(i['coordinates'])
-        if i['jointname'] == "WristLeft":
-            wristLeft = i['coordinates']
-            # print(i['coordinates'])
-        if i['jointname'] == "ShoulderRight":
-            shoulderRight = i['coordinates']
-            # print(i['coordinates'])
-        if i['jointname'] == "ElbowRight":
-            elbowRight = i['coordinates']
-            # print(i['coordinates'])
-        if i['jointname'] == "WristRight":
-            wristRight = i['coordinates']
-            # print(i['coordinates'])
-            # print(shoulderRight)
+    x=1
+    if 'name' in payload:
+        # for i in payload:
+        #     if i['coord'+x] == ""
+        print(len(payload)-1)
+        for key, value in payload.iteritems():
+            print (key, value)
+        
+    else:
+        for i in payload:        
+            if i['jointname'] == "ShoulderLeft": # checks jointname in json
+                shoulderLeft = i['coordinates'] # puts the corresponding coordinates in the global list
+            if i['jointname'] == "ElbowLeft": # checks jointname in json
+                elbowLeft = i['coordinates'] # puts the corresponding coordinates in the global list
+            if i['jointname'] == "WristLeft": # checks jointname in json
+                wristLeft = i['coordinates'] # puts the corresponding coordinates in the global list
+            if i['jointname'] == "ShoulderRight": # checks jointname in json
+                shoulderRight = i['coordinates'] # puts the corresponding coordinates in the global list
+            if i['jointname'] == "ElbowRight": # checks jointname in json
+                elbowRight = i['coordinates'] # puts the corresponding coordinates in the global list
+            if i['jointname'] == "WristRight": # checks jointname in json
+                wristRight = i['coordinates'] # puts the corresponding coordinates in the global list
 
             listAngles.append(
                 angleRShoulderPitch(shoulderRight[0], shoulderRight[1], shoulderRight[2], elbowRight[0], elbowRight[1],
-                                    elbowRight[2]))
+                                    elbowRight[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleRShoulderRoll(shoulderRight[0], shoulderRight[1], shoulderRight[2], elbowRight[0], elbowRight[1],
-                                   elbowRight[2]))
+                                   elbowRight[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleRElbowRoll(shoulderRight[0], shoulderRight[1], shoulderRight[2], elbowRight[0], elbowRight[1],
-                                elbowRight[2], wristRight[0], wristRight[1], wristRight[2]))
+                                elbowRight[2], wristRight[0], wristRight[1], wristRight[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleRElbowYaw(elbowRight[0], elbowRight[1], elbowRight[2], wristRight[0], wristRight[1],
                                wristRight[2], angleRShoulderPitch(shoulderRight[0], shoulderRight[1], shoulderRight[2], elbowRight[0], elbowRight[1],
-                                    elbowRight[2])))
+                                    elbowRight[2]))) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleLShoulderPitch(shoulderLeft[0], shoulderLeft[1], shoulderLeft[2], elbowLeft[0], elbowLeft[1],
-                                    elbowLeft[2]))
+                                    elbowLeft[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleLShouderRoll(shoulderLeft[0], shoulderLeft[1], shoulderLeft[2], elbowLeft[0], elbowLeft[1],
-                                   elbowLeft[2]))
+                                   elbowLeft[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleLElbowRoll(shoulderLeft[0], shoulderLeft[1], shoulderLeft[2], elbowLeft[0], elbowLeft[1],
-                                elbowLeft[2], wristLeft[0], wristLeft[1], wristLeft[2]))
+                                elbowLeft[2], wristLeft[0], wristLeft[1], wristLeft[2])) # calculates the angles via the Function with given coordinates and appends them to the masterlist
             listAngles.append(
                 angleLElbowYaw(elbowLeft[0], elbowLeft[1], elbowLeft[2], wristLeft[0], wristLeft[1],
                                wristLeft[2], angleLShoulderPitch(shoulderLeft[0], shoulderLeft[1], shoulderLeft[2], elbowLeft[0], elbowLeft[1],
-                                    elbowLeft[2])))
-    sendrobot(listAngles, raw_input("Robot ip adres: "), raw_input("Robot port (default: 9559): "))
-    # print("-----------------------------------")
+                                    elbowLeft[2]))) # calculates the angles via the Function with given coordinates and appends them to the masterlist
+        sendrobot(listAngles, raw_input("Robot ip adres: "), raw_input("Robot port (default: 9559): ")) # asks userinput to connect to the robot 
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
+client = mqtt.Client() # mqtt stuff
+client.on_connect = on_connect # mqtt stuff
 
-client.on_message = on_message
+client.on_message = on_message # mqtt stuff
 
 # client.connect("169.254.10.11", 1883, 60)
-client.connect(raw_input("Mqtt ip addres: "), 1883, 60)
-client.loop_forever()
+# client.connect(raw_input("Mqtt ip address: "), 1883, 60) # Asks mqtt ip address
+client.connect("52.174.68.36", 1883, 60) # Asks mqtt ip address
+client.loop_forever() # listen forever
